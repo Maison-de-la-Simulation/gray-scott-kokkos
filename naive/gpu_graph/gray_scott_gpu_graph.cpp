2b821944aab8f805fd2bd0a76a5299487bb91184
Thomas Padioleau <thomas.padioleau@cea.fr>
2026-04-27 16:44:21 +0200

    Add async and graph implementations

---
+++ naive/gpu_graph/gray_scott_gpu_graph.cpp
@@ -0,0 +1,172 @@
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Graph.hpp>
+
+#include "helpers.hpp"
+#include "output_writer.hpp"
+#include "parameters.hpp"
+
+// data type
+using real = double;
+
+/**
+ * @brief Constants for the Gray-Scott equation.
+ */
+namespace constants {
+
+constexpr real kill_rate{0.054};
+constexpr real feed_rate{0.014};
+constexpr real dt{1.0};
+constexpr real diffusion_rate_u{0.1};
+constexpr real diffusion_rate_v{0.05};
+
+}  // namespace constants
+
+// views type
+using View = Kokkos::View<real **>;
+
+/**
+ * @brief Add a drop at the center of the fields.
+ * @param u U field.
+ * @param v V field.
+ */
+void add_drop(const View &u, const View &v) {
+    const std::size_t n_rows_ext = u.extent(0);
+    const std::size_t n_columns_ext = u.extent(1);
+
+    // find drop location
+    // central cell + 1
+    const std::size_t i_center = n_rows_ext / 2;
+    const std::size_t j_center = n_columns_ext / 2;
+    const std::size_t i_drop_first = i_center - 1;
+    const std::size_t i_drop_last = i_center + 1;
+    const std::size_t j_drop_first = j_center - 1;
+    const std::size_t j_drop_last = j_center + 1;
+
+    Kokkos::parallel_for(
+        "add drop",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({i_drop_first, j_drop_first},
+                                               {i_drop_last, j_drop_last}),
+        KOKKOS_LAMBDA(const int i, const int j) {
+            u(i, j) = 0;
+            v(i, j) = 1;
+        });
+}
+
+/**
+ * @brief Compute the Gray-Scott equation for one iteration.
+ * @param u U field.
+ * @param v V field.
+ * @param u_temp U temporary field.
+ * @param v_temp V temporary field.
+ */
+Kokkos::Experimental::GraphNodeRef<Kokkos::DefaultExecutionSpace> compute(Kokkos::Experimental::GraphNodeRef<Kokkos::DefaultExecutionSpace> root, const View &u, const View &v, const View &u_temp,
+             const View &v_temp) {
+    const std::size_t n_rows_ext = u.extent(0);
+    const std::size_t n_columns_ext = u.extent(1);
+
+    return root.then_parallel_for(
+        "compute",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
+            {1, 1},
+            {n_rows_ext - 1, n_columns_ext - 1}),  // do not iterate on the halo
+        KOKKOS_LAMBDA(const int i, const int j) {
+            real u_full = 0;
+            real v_full = 0;
+            for (int k = -1; k <= 1; k++) {
+                for (int l = -1; l <= 1; l++) {
+                    u_full += u(i + k, j + l) - u(i, j);
+                    v_full += v(i + k, j + l) - v(i, j);
+                }
+            }
+
+            const real uvv = u(i, j) * v(i, j) * v(i, j);
+
+            const real u_delta = constants::diffusion_rate_u * u_full - uvv +
+                                 constants::feed_rate * (1 - u(i, j));
+            const real v_delta =
+                constants::diffusion_rate_v * v_full + uvv -
+                (constants::feed_rate + constants::kill_rate) * v(i, j);
+
+            u_temp(i, j) = u(i, j) + u_delta * constants::dt;
+            v_temp(i, j) = v(i, j) + v_delta * constants::dt;
+        });
+}
+
+int main(int argc, char *argv[]) {
+    Kokkos::ScopeGuard kokkos{argc, argv};
+
+    Parameters parameters{argc, argv};
+    parameters.describe();
+
+    // fields (with halo)
+    View u("u", parameters.n_rows_ext, parameters.n_columns_ext);
+    View v("v", parameters.n_rows_ext, parameters.n_columns_ext);
+
+    // mirrors of the fields (with halo)
+    auto u_h = Kokkos::create_mirror_view(u);
+    auto v_h = Kokkos::create_mirror_view(v);
+
+    // create writer
+    OutputWriter<real> writer(
+        "gray_scott.h5", parameters.n_iterations / parameters.images_interval,
+        parameters.n_rows_ext, parameters.n_columns_ext);
+
+    // initialize fields
+    Kokkos::deep_copy(u, 1);
+    Kokkos::deep_copy(v, 0);
+
+    // add a drop at the center of the domain
+    add_drop(u, v);
+
+    // transfer fields
+    Kokkos::deep_copy(u_h, u);
+    Kokkos::deep_copy(v_h, v);
+
+    // print init if requested
+    if (parameters.display_fields) {
+        helpers::print_field(u_h, 0);
+        helpers::print_field(v_h, 0);
+    }
+
+    // temporary fields (with halo)
+    View u_temp("u_temp", parameters.n_rows_ext, parameters.n_columns_ext);
+    View v_temp("v_temp", parameters.n_rows_ext, parameters.n_columns_ext);
+
+    auto compute_image = [&](Kokkos::Experimental::GraphNodeRef<Kokkos::DefaultExecutionSpace> node) {
+        for (int iteration = 0; iteration < parameters.images_interval; iteration++) {
+            node = compute(node, u, v, u_temp, v_temp);
+            Kokkos::kokkos_swap(u, u_temp);
+            Kokkos::kokkos_swap(v, v_temp);
+        }
+        node = node.then("", [&](){
+            Kokkos::deep_copy(Kokkos::DefaultExecutionSpace(), v_h, v);}
+        );
+        node.then_host("", [&](){
+                    writer.write(v_h.data());
+        });
+        return node;
+    };
+
+    auto graph = Kokkos::Experimental::create_graph(Kokkos::DefaultExecutionSpace(), [&](Kokkos::Experimental::GraphNodeRef<Kokkos::DefaultExecutionSpace> root) {
+        for (int image = 0; image < (parameters.n_iterations / parameters.images_interval); image++) {
+            root = compute_image(root);
+        }
+    });
+
+    graph.instantiate();
+    graph.submit();
+
+    // checksum
+    helpers::print_checksum(u, parameters.n_iterations);
+    helpers::print_checksum(v, parameters.n_iterations);
+
+    // transfer fields
+    Kokkos::deep_copy(u_h, u);
+    Kokkos::deep_copy(v_h, v);
+
+    // print last if requested
+    if (parameters.display_fields) {
+        helpers::print_field(u_h, parameters.n_iterations);
+        helpers::print_field(v_h, parameters.n_iterations);
+    }
+}
