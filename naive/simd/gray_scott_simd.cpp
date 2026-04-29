0c3544d85f7d496d2b292af4bed0759a119218e8
Paul Zehner <paul.zehner@cea.fr>
2026-04-29 15:18:41 +0200

    Apply code formatting

--- naive/simd/gray_scott_simd.cpp
+++ naive/simd/gray_scott_simd.cpp
@@ -19,7 +19,7 @@ constexpr real dt{1.0};
 constexpr real diffusion_rate_u{0.1};
 constexpr real diffusion_rate_v{0.05};
 
-} // namespace constants
+}  // namespace constants
 
 // views type
 using View = Kokkos::View<real **, Kokkos::LayoutRight>;
@@ -30,26 +30,26 @@ using View = Kokkos::View<real **, Kokkos::LayoutRight>;
  * @param v V field.
  */
 void add_drop(const View &u, const View &v) {
-  const std::size_t n_rows_ext = u.extent(0);
-  const std::size_t n_columns_ext = u.extent(1);
+    const std::size_t n_rows_ext = u.extent(0);
+    const std::size_t n_columns_ext = u.extent(1);
 
-  // find drop location
-  // central cell + 1
-  const std::size_t i_center = n_rows_ext / 2;
-  const std::size_t j_center = n_columns_ext / 2;
-  const std::size_t i_drop_first = i_center - 1;
-  const std::size_t i_drop_last = i_center + 1;
-  const std::size_t j_drop_first = j_center - 1;
-  const std::size_t j_drop_last = j_center + 1;
+    // find drop location
+    // central cell + 1
+    const std::size_t i_center = n_rows_ext / 2;
+    const std::size_t j_center = n_columns_ext / 2;
+    const std::size_t i_drop_first = i_center - 1;
+    const std::size_t i_drop_last = i_center + 1;
+    const std::size_t j_drop_first = j_center - 1;
+    const std::size_t j_drop_last = j_center + 1;
 
-  Kokkos::parallel_for(
-      "add drop",
-      Kokkos::MDRangePolicy<Kokkos::Rank<2>>({i_drop_first, j_drop_first},
-                                             {i_drop_last, j_drop_last}),
-      KOKKOS_LAMBDA(const int i, const int j) {
-        u(i, j) = 0;
-        v(i, j) = 1;
-      });
+    Kokkos::parallel_for(
+        "add drop",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2>>({i_drop_first, j_drop_first},
+                                               {i_drop_last, j_drop_last}),
+        KOKKOS_LAMBDA(const int i, const int j) {
+            u(i, j) = 0;
+            v(i, j) = 1;
+        });
 }
 
 /**
@@ -68,92 +68,92 @@ void add_drop(const View &u, const View &v) {
 template <typename SimdType>
 void compute_simd_kernel(const View &u, const View &v, const View &u_temp,
                          const View &v_temp, int start, int end) {
-  namespace KE = Kokkos::Experimental;
+    namespace KE = Kokkos::Experimental;
 
-  const int n_rows = u.extent(0);
-  const int n_cols = u.extent(1);
+    const int n_rows = u.extent(0);
+    const int n_cols = u.extent(1);
 
-  constexpr int simd_width = SimdType::size();
+    constexpr int simd_width = SimdType::size();
 
-  int const n_blocks = (end - start) / simd_width;
+    int const n_blocks = (end - start) / simd_width;
 
-  int const i_stride = u.stride(0);
+    int const i_stride = u.stride(0);
 
-  Kokkos::parallel_for(
-      "compute_simd",
-      Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Right>>(
-          {1, 0}, {n_rows - 1, n_blocks}),
+    Kokkos::parallel_for(
+        "compute_simd",
+        Kokkos::MDRangePolicy<Kokkos::Rank<2, Kokkos::Iterate::Right>>(
+            {1, 0}, {n_rows - 1, n_blocks}),
 
-      KOKKOS_LAMBDA(const int i, const int j_block) {
-        // starting column of SIMD lane block (contiguous j-dimension segment)
-        // j0 points to the first element of the SIMD block and advances in
-        // steps of simd_width
-        const int j0 = j_block * simd_width + start;
+        KOKKOS_LAMBDA(const int i, const int j_block) {
+            // starting column of SIMD lane block (contiguous j-dimension
+            // segment) j0 points to the first element of the SIMD block and
+            // advances in steps of simd_width
+            const int j0 = j_block * simd_width + start;
 
-        // flattened 2D index using the row stride of the LayoutRight view
-        // corresponds to the grid cell at position (i, j0)
-        const int base_center = i_stride * i + j0;
+            // flattened 2D index using the row stride of the LayoutRight view
+            // corresponds to the grid cell at position (i, j0)
+            const int base_center = i_stride * i + j0;
 
-        // Load SIMD registers for 3x3 stencil
+            // Load SIMD registers for 3x3 stencil
 
-        SimdType u_center(u.data() + base_center, KE::simd_flag_default);
-        SimdType v_center(v.data() + base_center, KE::simd_flag_default);
+            SimdType u_center(u.data() + base_center, KE::simd_flag_default);
+            SimdType v_center(v.data() + base_center, KE::simd_flag_default);
 
-        SimdType u_left(u.data() + base_center - 1, KE::simd_flag_default);
-        SimdType u_right(u.data() + base_center + 1, KE::simd_flag_default);
+            SimdType u_left(u.data() + base_center - 1, KE::simd_flag_default);
+            SimdType u_right(u.data() + base_center + 1, KE::simd_flag_default);
 
-        SimdType v_left(v.data() + base_center - 1, KE::simd_flag_default);
-        SimdType v_right(v.data() + base_center + 1, KE::simd_flag_default);
+            SimdType v_left(v.data() + base_center - 1, KE::simd_flag_default);
+            SimdType v_right(v.data() + base_center + 1, KE::simd_flag_default);
 
-        // Up and Down Center
-        const int base_up = base_center - i_stride;
-        const int base_down = base_center + i_stride;
+            // Up and Down Center
+            const int base_up = base_center - i_stride;
+            const int base_down = base_center + i_stride;
 
-        SimdType u_up(u.data() + base_up, KE::simd_flag_default);
-        SimdType u_down(u.data() + base_down, KE::simd_flag_default);
+            SimdType u_up(u.data() + base_up, KE::simd_flag_default);
+            SimdType u_down(u.data() + base_down, KE::simd_flag_default);
 
-        SimdType v_up(v.data() + base_up, KE::simd_flag_default);
-        SimdType v_down(v.data() + base_down, KE::simd_flag_default);
+            SimdType v_up(v.data() + base_up, KE::simd_flag_default);
+            SimdType v_down(v.data() + base_down, KE::simd_flag_default);
 
-        // Corners u
-        SimdType u_ul(u.data() + base_up - 1, KE::simd_flag_default);
-        SimdType u_ur(u.data() + base_up + 1, KE::simd_flag_default);
-        SimdType u_dl(u.data() + base_down - 1, KE::simd_flag_default);
-        SimdType u_dr(u.data() + base_down + 1, KE::simd_flag_default);
+            // Corners u
+            SimdType u_ul(u.data() + base_up - 1, KE::simd_flag_default);
+            SimdType u_ur(u.data() + base_up + 1, KE::simd_flag_default);
+            SimdType u_dl(u.data() + base_down - 1, KE::simd_flag_default);
+            SimdType u_dr(u.data() + base_down + 1, KE::simd_flag_default);
 
-        // Corners v
-        SimdType v_ul(v.data() + base_up - 1, KE::simd_flag_default);
-        SimdType v_ur(v.data() + base_up + 1, KE::simd_flag_default);
-        SimdType v_dl(v.data() + base_down - 1, KE::simd_flag_default);
-        SimdType v_dr(v.data() + base_down + 1, KE::simd_flag_default);
+            // Corners v
+            SimdType v_ul(v.data() + base_up - 1, KE::simd_flag_default);
+            SimdType v_ur(v.data() + base_up + 1, KE::simd_flag_default);
+            SimdType v_dl(v.data() + base_down - 1, KE::simd_flag_default);
+            SimdType v_dr(v.data() + base_down + 1, KE::simd_flag_default);
 
-        // Compute stencil
-        SimdType u_full =
-            (u_left + u_right + u_up + u_down + u_ul + u_ur + u_dl + u_dr) -
-            8 * u_center;
+            // Compute stencil
+            SimdType u_full =
+                (u_left + u_right + u_up + u_down + u_ul + u_ur + u_dl + u_dr) -
+                8 * u_center;
 
-        SimdType v_full =
-            (v_left + v_right + v_up + v_down + v_ul + v_ur + v_dl + v_dr) -
-            8 * v_center;
+            SimdType v_full =
+                (v_left + v_right + v_up + v_down + v_ul + v_ur + v_dl + v_dr) -
+                8 * v_center;
 
-        SimdType uvv = u_center * v_center * v_center;
+            SimdType uvv = u_center * v_center * v_center;
 
-        SimdType u_next =
-            u_center + (constants::diffusion_rate_u * u_full - uvv +
-                        constants::feed_rate * (1 - u_center)) *
-                           constants::dt;
+            SimdType u_next =
+                u_center + (constants::diffusion_rate_u * u_full - uvv +
+                            constants::feed_rate * (1 - u_center)) *
+                               constants::dt;
 
-        SimdType v_next =
-            v_center +
-            (constants::diffusion_rate_v * v_full + uvv -
-             (constants::feed_rate + constants::kill_rate) * v_center) *
-                constants::dt;
+            SimdType v_next =
+                v_center +
+                (constants::diffusion_rate_v * v_full + uvv -
+                 (constants::feed_rate + constants::kill_rate) * v_center) *
+                    constants::dt;
 
-        KE::simd_unchecked_store(u_next, u_temp.data() + base_center,
-                                 KE::simd_flag_default);
-        KE::simd_unchecked_store(v_next, v_temp.data() + base_center,
-                                 KE::simd_flag_default);
-      });
+            KE::simd_unchecked_store(u_next, u_temp.data() + base_center,
+                                     KE::simd_flag_default);
+            KE::simd_unchecked_store(v_next, v_temp.data() + base_center,
+                                     KE::simd_flag_default);
+        });
 }
 
 /**
@@ -165,84 +165,84 @@ void compute_simd_kernel(const View &u, const View &v, const View &u_temp,
  */
 void compute(const View &u, const View &v, const View &u_temp,
              const View &v_temp) {
-  namespace KE = Kokkos::Experimental;
+    namespace KE = Kokkos::Experimental;
 
-  using simd_t = KE::simd<real>;
-  using simd_scalar_t = KE::basic_simd<real, KE::simd_abi::scalar>;
+    using simd_t = KE::simd<real>;
+    using simd_scalar_t = KE::basic_simd<real, KE::simd_abi::scalar>;
 
-  const auto simd_width = simd_t::size();
+    const auto simd_width = simd_t::size();
 
-  const int n_cols = u.extent(1);
+    const int n_cols = u.extent(1);
 
-  const int j_begin = 1;
-  const int interior = n_cols - 2;
+    const int j_begin = 1;
+    const int interior = n_cols - 2;
 
-  const int vec_end = j_begin + (interior / simd_width) * simd_width;
+    const int vec_end = j_begin + (interior / simd_width) * simd_width;
 
-  // SIMD
-  compute_simd_kernel<simd_t>(u, v, u_temp, v_temp, j_begin, vec_end);
+    // SIMD
+    compute_simd_kernel<simd_t>(u, v, u_temp, v_temp, j_begin, vec_end);
 
-  // Scalar tail
-  if (vec_end < n_cols - 1) {
-    compute_simd_kernel<simd_scalar_t>(u, v, u_temp, v_temp, vec_end,
-                                       n_cols - 1);
-  }
+    // Scalar tail
+    if (vec_end < n_cols - 1) {
+        compute_simd_kernel<simd_scalar_t>(u, v, u_temp, v_temp, vec_end,
+                                           n_cols - 1);
+    }
 }
 
 int main(int argc, char *argv[]) {
-  Kokkos::ScopeGuard kokkos{argc, argv};
+    Kokkos::ScopeGuard kokkos{argc, argv};
 
-  Parameters parameters{argc, argv};
-  parameters.describe();
+    Parameters parameters{argc, argv};
+    parameters.describe();
 
-  // fields (with halo)
-  View u("u", parameters.n_rows_ext, parameters.n_columns_ext);
-  View v("v", parameters.n_rows_ext, parameters.n_columns_ext);
+    // fields (with halo)
+    View u("u", parameters.n_rows_ext, parameters.n_columns_ext);
+    View v("v", parameters.n_rows_ext, parameters.n_columns_ext);
 
-  // create writer
-  OutputWriter<real> writer(
-      "gray_scott.h5", parameters.n_iterations / parameters.images_interval,
-      parameters.n_rows_ext, parameters.n_columns_ext);
+    // create writer
+    OutputWriter<real> writer(
+        "gray_scott.h5", parameters.n_iterations / parameters.images_interval,
+        parameters.n_rows_ext, parameters.n_columns_ext);
 
-  // initialize fields
-  Kokkos::deep_copy(u, 1);
-  Kokkos::deep_copy(v, 0);
+    // initialize fields
+    Kokkos::deep_copy(u, 1);
+    Kokkos::deep_copy(v, 0);
 
-  // add a drop at the center of the domain
-  add_drop(u, v);
+    // add a drop at the center of the domain
+    add_drop(u, v);
 
-  // print init if requested
-  if (parameters.display_fields) {
-    helpers::print_field(u, 0);
-    helpers::print_field(v, 0);
-  }
-
-  // write init
-  writer.write(v.data());
-
-  // temporary fields (with halo)
-  View u_temp("u_temp", parameters.n_rows_ext, parameters.n_columns_ext);
-  View v_temp("v_temp", parameters.n_rows_ext, parameters.n_columns_ext);
-
-  // time loop
-  for (int iteration = 0; iteration < parameters.n_iterations; iteration++) {
-    compute(u, v, u_temp, v_temp);
-    Kokkos::kokkos_swap(u, u_temp);
-    Kokkos::kokkos_swap(v, v_temp);
-
-    // write image every images_interval iterations
-    if (iteration % parameters.images_interval == 0) {
-      writer.write(v.data());
+    // print init if requested
+    if (parameters.display_fields) {
+        helpers::print_field(u, 0);
+        helpers::print_field(v, 0);
     }
-  }
 
-  // checksum
-  helpers::print_checksum(u, parameters.n_iterations);
-  helpers::print_checksum(v, parameters.n_iterations);
+    // write init
+    writer.write(v.data());
 
-  // print last if requested
-  if (parameters.display_fields) {
-    helpers::print_field(u, parameters.n_iterations);
-    helpers::print_field(v, parameters.n_iterations);
-  }
+    // temporary fields (with halo)
+    View u_temp("u_temp", parameters.n_rows_ext, parameters.n_columns_ext);
+    View v_temp("v_temp", parameters.n_rows_ext, parameters.n_columns_ext);
+
+    // time loop
+    for (int iteration = 0; iteration < parameters.n_iterations; iteration++) {
+        compute(u, v, u_temp, v_temp);
+        Kokkos::kokkos_swap(u, u_temp);
+        Kokkos::kokkos_swap(v, v_temp);
+
+        // write image every images_interval iterations
+        if (iteration % parameters.images_interval == 0) {
+            writer.write(v.data());
+        }
+    }
+
+    // checksum
+    helpers::print_checksum(u, parameters.n_iterations);
+    helpers::print_checksum(v, parameters.n_iterations);
+
+    // print last if requested
+    if (parameters.display_fields) {
+        helpers::print_field(u, parameters.n_iterations);
+        helpers::print_field(v, parameters.n_iterations);
+    }
 }
