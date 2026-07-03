c346e9a477dcb1a1b31baec711ddae8af54895e0
Paul Zehner <paul.zehner@cea.fr>
2026-07-03 10:42:27 +0200

    Allow to set precision from CMake

--- exercises/common.cmake
+++ exercises/common.cmake
@@ -1,4 +1,10 @@
 include_guard()
 
+option(
+    ENABLE_DOUBLE
+    "Enable double precision for floating variables, instead of simple precision"
+    ON
+)
+
 # add the common libs from top level or low level
 add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/common" "${CMAKE_CURRENT_BINARY_DIR}/common")
